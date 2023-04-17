634afb0eba31801317b4b70aa3bf6339cae9cb2d
Sanchay Javeria <sanchay.javeria@gmail.com>
2023-04-16 22:14:23 -0700

    Add UI form and API fetch logic

---
+++ shepherd-client/components/apiHandlerButton.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+interface Response {
+    msg: string;
+}
+
+export interface FormData {
+    isoCode: string;
+    yearsOfExperience: number;
+    state: string;
+    techUsageGrade: number;
+}
+
+export default function ApiHandlerButton({params}: {params: FormData}) {
+    const [data, setData] = useState<Response | null>(null);
+
+    const queryParams = new URLSearchParams();
+    for (const [key, value] of Object.entries(params)) {
+        queryParams.append(key, value);
+    }
+
+    const fetchData = async () => {
+        const res = await fetch(`/api/model?${queryParams.toString}`);
+        const json = await res.json();
+        setData(json);
+    };
+
+    return (
+        <div>
+            <button onClick={fetchData}>Fetch Data</button>
+            {data && data.msg}
+        </div>
+    );
+}
--- shepherd-client/pages/index.tsx
+++ shepherd-client/pages/index.tsx
@@ -1,20 +1,85 @@
-import React, { useRef } from 'react'
-import styles from '../styles/styles.module.css'
+import { useState } from 'react';
+import styles from '../styles/styles.module.css';
+import { StateSelect } from '../components/stateSelect';
+import ApiHandlerButton, { FormData } from '../components/apiHandlerButton';
 
-export default function App() {
+export default function Form() {
+    const [formData, setFormData] = useState<FormData>({
+        isoCode: "",
+        yearsOfExperience: 0,
+        state: "",
+        techUsageGrade: 0,
+    });
 
-    const Footer = () => (
-        <div className={`${styles.footer}`}>
-            <p>
-                This is a footer!
-            </p>
-        </div>
-    )
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(formData);
+    };
 
     return (
-        <div style={{ background: '#dfdfdf' }}>
-            <h1>Welcome!</h1>
-            <Footer />
-        </div>
-    )
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>
+                    ISO Code:
+                    <input
+                        type="text"
+                        value={formData.isoCode}
+                        onChange={(e) =>
+                            setFormData((prevFormData) => ({
+                                ...prevFormData,
+                                isoCode: e.target.value,
+                            }))
+                        }
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Years of Experience:
+                    <input
+                        type="number"
+                        value={formData.yearsOfExperience}
+                        onChange={(e) =>
+                            setFormData((prevFormData) => ({
+                                ...prevFormData,
+                                yearsOfExperience: parseInt(e.target.value),
+                            }))
+                        }
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    State:
+                    <StateSelect
+                        value={formData.state}
+                        onChange={(value) =>
+                            setFormData((prevFormData) => ({
+                                ...prevFormData,
+                                state: value,
+                            }))
+                        }
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Tech Usage Grade:
+                    <input
+                        type="number"
+                        value={formData.techUsageGrade}
+                        onChange={(e) =>
+                            setFormData((prevFormData) => ({
+                                ...prevFormData,
+                                techUsageGrade: parseInt(e.target.value),
+                            }))
+                        }
+                    />
+                </label>
+            </div>
+            <div>
+                <ApiHandlerButton params={formData}></ApiHandlerButton>
+            </div>
+        </form>
+    );
 }
